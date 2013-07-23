e7bf5e2875388161346f6c6bfc7b4a53dd9ef4f9
allen.yao <allen.yao@quester.com.cn>
2013-07-23 13:52:57 +0800

    Add brandcom wifi

--- sparkauto_6dq/BoardConfig.mk
+++ sparkauto_6dq/BoardConfig.mk
@@ -11,15 +11,25 @@ include device/fsl/sparkauto_6dq/vendor.mk
 BUILD_TARGET_FS ?= ext4
 include device/fsl/imx6/imx6_target_fs.mk
 
+WIFI_BAND := 802_11_BG
+include hardware/broadcom/wlan/bcmdhd/firmware/bcm4330/device-bcm.mk
+
 PREBUILT_3G_MODEM_RIL			:= true
 TARGET_BOOTLOADER_BOARD_NAME := sparkauto
 PRODUCT_MODEL := SPARKAUTO-MX6DQ
 
-# Wifi FIXME
-WPA_SUPPLICANT_VERSION				:= VER_0_8_X
-BOARD_WPA_SUPPLICANT_DRIVER      	:= NL80211
-BOARD_HOSTAPD_DRIVER             	 	:= NL80211
-WIFI_TEST_INTERFACE			 		:= "sta"
+
+
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+WPA_SUPPLICANT_VERSION := VER_0_8_X
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+BOARD_HOSTAPD_DRIVER := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+BOARD_WLAN_DEVICE := bcmdhd
+WIFI_DRIVER_FW_PATH_PARAM := "/sys/module/bcmdhd/parameters/firmware_path"
+WIFI_DRIVER_FW_PATH_STA := "/vendor/firmware/fw_bcmdhd.bin"
+WIFI_DRIVER_FW_PATH_AP := "/vendor/firmware/fw_bcmdhd_apsta.bin"
+WIFI_DRIVER_FW_PATH_P2P := "/vendor/firmware/fw_bcmdhd_p2p.bin"
 
 
 #BT FIXME
