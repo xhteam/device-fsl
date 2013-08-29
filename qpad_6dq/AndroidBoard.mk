d59e2390a37b1d426fbea4e3880e9b873c7240a1
raymond.wang <raymond1860@gmail.com>
2013-08-29 10:06:04 +0800

    Move BOARD configuratin BOARD_HAS_SENSOR to fsl-proprietary/sensor/fsl-sensor.mk, so all product of legacy freescale does not require any change. ifeq ($(BOARD_HAS_SENSOR),true) xxx endif

--- qpad_6dq/AndroidBoard.mk
+++ qpad_6dq/AndroidBoard.mk
@@ -4,6 +4,4 @@ ifeq ($(PREBUILT_FSL_IMX_CODEC),true)
 include device/fsl-proprietary/codec/fsl-codec.mk
 endif
 
-ifeq ($(BOARD_HAS_SENSOR),true)
 include device/fsl-proprietary/sensor/fsl-sensor.mk
-endif
