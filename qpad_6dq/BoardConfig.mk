a9e5f8a000bd3f224a86754049b91a9565783f01
raymond.wang <raymond1860@gmail.com>
2013-10-31 17:29:23 +0800

    For system power saving,we reduce cpus cores to 2 even on iMX6Q.

--- qpad_6dq/BoardConfig.mk
+++ qpad_6dq/BoardConfig.mk
@@ -59,7 +59,7 @@ TARGET_SELECT_KEY := 28
 TARGET_USERIMAGES_SPARSE_EXT_DISABLED := true
 
 
-BOARD_KERNEL_CMDLINE := console=ttymxc0,115200 init=/init video=mxcfb0:dev=mipi_dsi video=mxcfb1:off video=mxcfb2:off fbmem=10M fb0base=0x27b00000 vmalloc=400M androidboot.console=ttymxc0 androidboot.hardware=freescale enable_wait_mode=off
+BOARD_KERNEL_CMDLINE := console=ttymxc0,115200 init=/init video=mxcfb0:dev=mipi_dsi video=mxcfb1:off video=mxcfb2:off fbmem=10M fb0base=0x27b00000 vmalloc=400M androidboot.console=ttymxc0 androidboot.hardware=freescale enable_wait_mode=off maxcpus=2
 
 
 USE_ION_ALLOCATOR := false
